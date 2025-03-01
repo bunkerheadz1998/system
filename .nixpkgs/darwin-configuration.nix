9f433886b15a36d3c841a64ef2e8d6dff1619ff1
bunkerheadz1998 <79599141+bunkerheadz1998@users.noreply.github.com>
2025-03-02 00:58:14 +0100

    Bump darwin-configuration.nix

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -1,98 +1,161 @@
 { config, pkgs, ... }:
 
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+	imports = [ <home-manager/nix-darwin> ];
 
-  # Keyboard
-  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
-  system.defaults.NSGlobalDomain.InitialKeyRepeat = 10;
-  system.defaults.NSGlobalDomain.KeyRepeat = 1;
-  system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
-  system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false; system.defaults.NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
-  system.defaults.NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
-  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
-  
-  # Ctrl remap
-  system.keyboard.enableKeyMapping = true;
-  system.keyboard.remapCapsLockToControl = true;
+	system.stateVersion = 5;
 
-  # Trackpad
-  #system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
-  #system.defaults.NSGlobalDomain."com.apple.trackpad.enableSecondaryClick" = true;
-  #system.defaults.NSGlobalDomain."com.apple.trackpad.trackpadCornerClickBehavior" = 1;
-  #system.defaults.NSGlobalDomain."com.apple.springing.enabled" = true;
-  #system.defaults.NSGlobalDomain."com.apple.springing.delay" = 0.0;
-  #system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = true;
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Dock 
-  system.defaults.dock.autohide = true;
+	system.defaults = {
+		NSGlobalDomain = { # Key repeat
+				ApplePressAndHoldEnabled = false;
+				InitialKeyRepeat = 10;
+				KeyRepeat = 1;
+				NSAutomaticCapitalizationEnabled = false;
+				NSAutomaticDashSubstitutionEnabled = false;
+				NSAutomaticPeriodSubstitutionEnabled = false;
+				NSAutomaticQuoteSubstitutionEnabled = false;
+				NSAutomaticSpellingCorrectionEnabled = false;
 
-  # Taskbar
-  system.defaults.NSGlobalDomain._HIHideMenuBar = true;
+				# Trackpad
+				"com.apple.mouse.tapBehavior" = 1;
+				"com.apple.trackpad.enableSecondaryClick" = true;
+				"com.apple.trackpad.trackpadCornerClickBehavior" = 1;
+				"com.apple.springing.enabled" = true;
+				"com.apple.springing.delay" = 0.0;
+				"com.apple.swipescrolldirection" = true;
 
-  # GUI
-  system.defaults.NSGlobalDomain.AppleShowAllFiles = true;
-  system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+				# Zoom
+				#"com.apple.universalaccess.closeViewHotkeysEnabled" = true;
 
-  # Misc
-  system.defaults.smb.NetBIOSName = "System";
-  system.defaults.smb.ServerDescription = "System";
-  system.defaults.screencapture.location = "/tmp";
+				# Autohide taskbar
+				_HIHideMenuBar = true;
 
-  environment.variables.EDITOR = "nvim";
+				# Show dotfiles et al
+				AppleShowAllFiles = true;
+				AppleShowAllExtensions = true;
+		};
 
-# List packages installed in system profile. To search by name, run:
-# $ nix-env -qaP | grep wget
-	environment.systemPackages =[
+		# Networking
+		smb = {
+			NetBIOSName = "System";
+			ServerDescription = "System";
+		};
+
+		# Set screenshot folder to /tmp
+		screencapture.location = "/tmp";
+
+		# Autohide dock
+		dock = {
+			autohide = true;
+		};
+	};
+
+	# Ctrl/shift remap
+	system.keyboard = {
+		enableKeyMapping = true;
+		remapCapsLockToControl = true;
+	};
+
+
+	# Env vars
+	environment.variables.EDITOR = "nvim";
+
+	# TODO Singal, Stubby, Wireguard, yt-dlp
+	#pkgs.cargo
+	#pkgs.rustc
+	#pkgs.stubby
+	#pkgs.urbit
+	#pkgs.signal-desktop
+	#pkgs.ungoogled-chromium
+
+	# List packages installed in system profile. To search by name, run:
+	# $ nix-env -qaP | grep wget
+	environment.systemPackages = [
+			pkgs.wget
+			pkgs.libusb1
 			pkgs.git
 			pkgs.curl
 			pkgs.gnupg
 			pkgs.neovim
 			pkgs.tmux
 			pkgs.yabai
+			pkgs.skhd
 			pkgs.ncurses
 			pkgs.nodejs
 			pkgs.yarn
 			pkgs.pwgen
-			pkgs.python38
 			pkgs.jupyter
 			pkgs.kitty
 			pkgs.docker
 			pkgs.htop
 			pkgs.ffmpeg
-			pkgs.cargo
+			pkgs.rustup
 			pkgs.jq
 			pkgs.imagemagick
 			pkgs.fzf
 			pkgs.fd
 			pkgs.tailscale
-			pkgs.skhd
 			pkgs.chicken
-			pkgs.youtube-dl
+			#pkgs.youtube-dl
 			pkgs.qrencode
 			pkgs.git-lfs
 			pkgs.go-ethereum
 			pkgs.libreoffice-bin
 			pkgs.testdisk
-			#pkgs.urbit
-			pkgs.keepassxc
-			#pkgs.signal-desktop
-			#pkgs.ungoogled-chromium
+			pkgs.python312Packages.optimum
+			pkgs.onnxruntime
+			#pkgs.php
+			#pkgs.keepassxc
+			#pkgs.yt-dlp
+			pkgs.protobuf
+			pkgs.cmake
+			#pkgs.conda
+			#pkgs.nmap
+			#pkgs.bun
+			#pkgs.coreutils
+			##pkgs.monero-gui
+			#pkgs.go
+			#pkgs.exiftool
+			pkgs.exif
+			#pkgs.stripe-cli
+			#pkgs.ghostscript
+			##pkgs.android-tools
+			#pkgs.pdftk
+			#pkgs.gallery-dl
+			pkgs.ifuse
+			pkgs.libimobiledevice
+			pkgs.libimobiledevice-glue
+			pkgs.libusb1
+			pkgs.libirecovery
 		];
 
-# Use a custom configuration.nix location.
-# $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-# environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+	# Use a custom configuration.nix location.d
+	# $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+	# environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
-# Auto upgrade nix package and the daemon service.
+	# Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 
+	# Yabai
+	#services.yabai.enable = true;
+
+	#services.skhd.enable = true;
+
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
+
+	#programs.git.enable = true;
+	#	enableCredentialCache = false; # Disable credential caching
+	#	extraConfig = {
+  #    credential.helper = "";  # Disable the credential helper
+  #  };
+  #};
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 4;
+  #system.stateVersion = 5;
 }
